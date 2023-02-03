b5f1190c921c64849bf9117dee75332e781c24ff
Nathalieelizabeth <elizabethsnathalie@gmail.com>
2023-02-03 08:21:47 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    var fullname : String = "NanaJackie"
+    println(fullname)
+    var Age : Byte = 22
+    println(Age)
+    var phonenumber:Long = 789654534
+    println(phonenumber)
+    var weightkg : Short = 60
+    println(weightkg)
+    var citizenkenyan: Boolean = true
+    println(citizenkenyan)
+
+
+}
